4c7fb245d58a5b6dee4530d940eab285aa0f96fe
wtser <wangtieshou@gmail.com>
2020-04-26 01:12:41 +0800

    Add doc

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,21 @@
 module.exports = {
   plugins: {
     "postcss-import": {},
-    "postcss-preset-env": {
-      stage: 1,
-      preserve: false,
-      importFrom: "./root.css",
-    },
+
     // "postcss-nested-import": {},
     // "postcss-url": {
     //   url: "inline",
     // },
     "postcss-for": {},
     "postcss-each": {},
+    "postcss-preset-env": {
+      stage: 1,
+      preserve: false,
+      importFrom: "./root.css",
+    },
+    "postcss-nested": {},
 
     "postcss-extend": {},
-    // "postcss-nested": {},
     // "postcss-will-change": {},
     // "postcss-color-function": {},
     // "postcss-calc": {},
